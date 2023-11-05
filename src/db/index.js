4afa40fe136af7bccbc4312ebeded636a6e3f915
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-11-05 23:15:41 +0530

    Added teh Connection of Database files

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`\n MongDB Connected..!! :${connectionInstance.connection.host}`);
+    } catch(error) {
+        console.log("MONGO_DB connection Error ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
