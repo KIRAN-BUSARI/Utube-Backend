7c596db7edf0cd1562c017c663b26338d5474618
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-11-11 23:05:53 +0530

    Added the user and video Model

---
+++ src/models/user.model.js
@@ -0,0 +1,64 @@
+import mongoose, { Schema } from "mongoose";
+import Jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        fullname: {
+            type: String,
+            required: false,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String,
+            required: true,
+        },
+        coverImage: {
+            type: String,
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId, ref:
+                "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is required."],
+            minlength: 8
+        },
+        refreshTokens: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrext = async function (password) {
+    return await bcrypt.compare(password,this.password)
+}
+
+export const User = mongoose.model("User",userSchema)
