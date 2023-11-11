fdb2f23f3b85e97728fb53ee36dd632fdc0ddce2
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-11-11 23:19:43 +0530

    Modified the user model with jwt package

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema = new Schema(
             lowercase: true,
             trim: true
         },
-        fullname: {
+        fullName: {
             type: String,
             required: false,
             trim: true,
@@ -45,6 +45,7 @@ const userSchema = new Schema(
         refreshTokens: {
             type: String
         }
+
     },
     {
         timestamps: true
@@ -59,6 +60,32 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordCorrext = async function (password) {
     return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return Jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function () {
+    return Jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    );
 }
 
 export const User = mongoose.model("User",userSchema)
