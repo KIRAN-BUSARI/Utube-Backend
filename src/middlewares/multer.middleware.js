a80867cadcf33d80aff33f7ab8d9d2095784c9ba
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-11-13 21:37:36 +0530

    Added the file destination path correctly

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -3,7 +3,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "/tmp/my-uploads");
+        cb(null, "../../public/temp");
     },
     filename: function (req, file, cb) {
         // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9); We can add theis later for the detailed understanding of the file upload We are adding it later.
