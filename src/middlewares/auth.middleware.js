efd3d1f0dd70e9960f68e3a4649419b249e9a0ac
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2023-12-18 22:28:00 +0530

    Fixed with some syntax error

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 /* This is middleware to check weather the user is present or not */
 import { User } from "../models/user.model.js";
-import { ApiError } from "../utils/ApiError";
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 
