3511d10a7f489086ed497121a3a2d6ebe4e40563
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-02-03 06:38:03 +0530

    Add the routes and fix typo

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
+import {
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
+    loginUser, logoutUser,
+    refreshAccessToken,
+    registerUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,10 +35,15 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/getUser").get(verifyJWT, getCurrentUser)
-router.route("/changePassword").get(verifyJWT, changeCurrentPassword)
-router.route("/updateAccountDetails").get(verifyJWT, updateAccountDetails)
-router.route("/updateCurrentAvatar").get(verifyJWT, updateUserAvatar)
-router.route("/updateCoverImage").get(verifyJWT, updateUserCoverImage)
-router.route("/getUserChannelProfile").get(verifyJWT, getUserChannelProfile)
+router.route("/changePassword").post(verifyJWT, changeCurrentPassword)
+
+router.route("/updateAccountDetails").patch(verifyJWT, updateAccountDetails)
+
+router.route("/updateCurrentAvatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/updateCoverImage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router;
